516f503e4adc899d3ea01c9d0a5e82b293c30cdd
sodiel3e3 <eunseo061113@gmail.com>
2025-10-01 13:15:48 +0900

    2025.10.01

---
+++ 조은서/1019_44905827(AC).c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+int main()
+{
+    int y, m, d;
+    
+    scanf("%d.", &y);
+    printf("%04d.", y);
+    scanf("%d.", &m);
+    printf("%02d.", m);
+    scanf("%d", &d);
+    printf("%02d\n", d);
+    
+    return 0;
+}
